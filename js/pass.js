5340adc1f0ff07857d37cf799719ba452bf7a7b5
Anthony <elmorodjeur@gmail.com>
2021-10-21 12:52:05 +0200

    MAJ classement

--- js/pass.js
+++ js/pass.js
@@ -1,14 +1,17 @@
+
 document.getElementById("inputPass").focus();
+
+
 
 function calendarPassword(){
     var pass = document.getElementById('inputPass').value;
     console.log(pass);
 
-    if (pass=="reims" || pass=="Reims" || pass=="Reims " || pass=="reims " ){
-        console.log("gagné");
-        document.getElementById("link").setAttribute(
-            "style", "display: flex;");
+    if (pass=="reims" || pass=="Reims" || pass=="Reims " || pass=="reims " ){     
+            console.log("gagné");
+            document.getElementById("link").setAttribute(
+            "style", "display: flex;"); 
     }
-
+    
 }
 
